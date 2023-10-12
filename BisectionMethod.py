288a6a383b4e1785b7c48d6f2ccb22b35c917f4c
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2023-10-12 21:55:04 +0000

    function in and main template

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -1,5 +1,30 @@
 import numpy as np
-import sympy as sp
+"""
+Module Docstring
+"""
 
-x = sp.symbols('x')
-f = sp.Eq(x**2-3)
+__author__ = "Trey Gower"
+
+def func(x):
+    """ The function provided used to calculate f(x) """
+    f = (x**2) - 3
+    return f
+
+def bisect_Method():
+    """ The bisection method
+     
+     Args:
+
+     Returns: 
+    
+    """
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
