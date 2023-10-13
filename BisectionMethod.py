e4001292e19af442873dcc5d4ec6ae6c0fe7a832
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2023-10-13 14:32:52 +0000

    Final Bisection Method Complete

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -10,20 +10,58 @@ def func(x):
     f = (x**2) - 3
     return f
 
-def bisect_Method():
+def bisect_Method(tol, maxit, ends):
     """ The bisection method
      
-     Args:
+     Args: A function f(x)
 
-     Returns: 
-    
+     Returns: The root of a polynomial 
     """
+    #getting the endpoints from the array
+    a = ends[0]
+    b = ends[1]
+    #iterations initialized
+    it = 0
+
+    #check endpoints for opposite signs
+    fb = func(b)
+    fa = func(a)
+
+    if np.sign(fa) == np.sign(fb):
+        return "Error: endpoints must have opposite signs"
+
+    while it <= maxit:
+        #midpoint calculation
+        c = (a+b)/2
+
+        #calculate function values
+        fc = func(c)
+        fa = func(a)
+
+        if fc == 0 or (b-a)/2 < tol:
+            root = "\nRoot found x = " + str(c) + " in " + str(it) + " Iterations\n"
+            return root
+        
+        #increment number of iterations
+        it = it+1
+
+        #assign closer endpoints for next iteration by intermediate value theorem
+        if np.sign(fc) == np.sign(fa):
+            a = c
+        else:
+            b = c
+    return "Bisection Method failed by exceeding max steps"
+
 
 
 def main():
     """ Main entry point of the app """
-    print("hello world")
+    tol = 1e-06
+    maxit = 50
+    ends = np.array([1,2])
+    bisect = bisect_Method(tol, maxit, ends)
 
+    print(bisect)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
