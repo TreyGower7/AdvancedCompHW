d22c8c89639f9d912caa3cd9297886bcfb3dec41
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2023-11-06 21:03:04 +0000

    Almost

--- hw5_FEM_BVP/BVP_Galerkin.py
+++ hw5_FEM_BVP/BVP_Galerkin.py
@@ -8,38 +8,60 @@ Solving the boundary value problem with
 where
     y(0) = y(1) = 0
 """
-
-#Let N = 3 as in problem statement
-N = 3
-#boundary condtions
-a=0
-b=1
-
 #define my basis function
 def Basis(i,x):
     return np.sin(i*np.pi*x)
 
-def dBasis(i,x):
-    return i*np.pi*np.sin(i*np.pi*x)
+#define my basis function
+def Basis_deriv(i,x):
+    return i*np.pi*np.cos(i*np.pi*x)
 
-def K_and_fvec():
-    """ Calculate Stiffness and load vector via the Galerkin method
+def Galerkin_kf(N,a,b):
+    """ Calculate Stiffness and load vector via the Galerkin method and solve ku=f
      
      Args:
 
      Returns: Stiffness matrix (K_ij) and load vector (f_i)
     
     """
-    K = np.zeros(N,N)
-    f = np.zeros(1,N)
+    K = np.zeros((N,N))
+    f = np.zeros((N))
+    for i in range(N):
+        integrand_rhs = lambda x: x * Basis_deriv(i,x)
+        # a =0 and b =1
+        f[i] = quad(integrand_rhs,a,b)[0]
+        for j in range(N):
+            integrand_lhs = lambda x: Basis_deriv(i,x) * Basis_deriv(j,x)
+            # a =0 and b =1
+            K[i][j] = quad(integrand_lhs,a,b)[0]
+    
+    return [K,f]
 
-    sumresult=0
-    for i in range(0, N):
-        sumresult += Basis(i,x)*dBasis(i,x)
+def trial_function(coeff,x,N):
+    soln = 0.0
+    for i in range(1, N+1):
+        soln += coeff[i-1] * Basis(i, x)
+    return soln
 
 def main():
     """ Main entry point of the script """
-    solve_kf = K_and_fvec()
+    #Let N = 3 as in problem statement
+    N = 3
+    #boundary condtions
+    x_val =.5
+    a=0
+    b=1
+    Kf = Galerkin_kf(N,a,b)
+
+    print(Kf[0])
+    print(Kf[1])
+
+    #solve for the coefficients
+    coeff = np.linalg.solve(Kf[0],Kf[1])
+    #calculate approximate solution given an x value
+    approximate = trial_function(coeff, x_val, N)
+
+    print(approximate + ' at ' + ' x = ' + str(x_val))
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
